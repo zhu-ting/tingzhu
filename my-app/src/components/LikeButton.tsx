dba70799f6578621b4ec7e9342b0a7228a174ab5
zhu-ting <850832627@qq.com>
2021-05-19 19:39:11 +1000

    Add toggle button

--- my-app/src/components/LikeButton.tsx
+++ my-app/src/components/LikeButton.tsx
@@ -1,12 +1,17 @@
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const LikeButton: React.FC = () => {
-    const [ like, setLike ] = useState(0);
+    const [ obj, setObj ] = useState({ like: 0, on: true });
 
     return (
-        <button onClick={() => {setLike(like + 1)}}>
-            {like} 👍
-        </button>
+        <Fragment>
+            <button onClick={() => {setObj({ like: obj.like + 1, on: obj.on })}}>
+                {obj.like} 👍
+            </button>
+            <button onClick={() => {setObj({ like: obj.like + 1, on: !obj.on })}}>
+                {obj.on ? 'ON' : 'OFF'}
+            </button>
+        </Fragment>
     )
 }
 
