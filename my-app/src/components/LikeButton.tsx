ff3bacb17ce7c5dd9be6cfbcc2574eef875aa67c
zhu-ting <850832627@qq.com>
2021-05-19 19:41:32 +1000

    Alternative useState

--- my-app/src/components/LikeButton.tsx
+++ my-app/src/components/LikeButton.tsx
@@ -1,15 +1,16 @@
 import React, { Fragment, useState } from 'react';
 
 const LikeButton: React.FC = () => {
-    const [ obj, setObj ] = useState({ like: 0, on: true });
+    const [ like, setLike ] = useState(0);
+    const [ on, setOn ] = useState(true);
 
     return (
         <Fragment>
-            <button onClick={() => {setObj({ like: obj.like + 1, on: obj.on })}}>
-                {obj.like} 👍
+            <button onClick={() => {setLike(like + 1)}}>
+                {like} 👍
             </button>
-            <button onClick={() => {setObj({ like: obj.like + 1, on: !obj.on })}}>
-                {obj.on ? 'ON' : 'OFF'}
+            <button onClick={() => {setOn(!on)}}>
+                {on ? 'ON' : 'OFF'}
             </button>
         </Fragment>
     )
