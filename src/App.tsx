8d13d1b8045829e923ef0952298db7fc73fe1509
zhu-ting <850832627@qq.com>
2021-05-23 12:36:47 +1000

    Set up with test Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button, { ButtonSize, ButtonType } from './Button/button';
 
 function App() {
   return (
@@ -10,6 +11,11 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <div>
+          <Button disabled>Button</Button>
+          <Button btnType={ButtonType.Default}>Button</Button>
+          <Button btnType={ButtonType.Danger}>Button</Button>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/Button/button.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import classnames from 'classnames';
+
+export enum ButtonSize {
+    Large = 'lg',
+    Small = 'sm'
+}
+
+export enum ButtonType {
+    Primary = 'primary',
+    Default = 'default',
+    Danger = 'danger',
+    Link = 'link'
+}
+
+interface BaseButtonProps {
+    className?: string;
+    disabled?: boolean;
+    size?: ButtonSize;
+    btnType?: ButtonType;
+    href?: string;
+    children: React.ReactNode
+}
+
+const Button: React.FC<BaseButtonProps> = (props) => {
+    const { btnType, disabled, children, size } = props;
+
+    // btn, btn-lg, btn-sm
+    const classes = classnames('btn', {
+        [`btn-${btnType}`]: btnType,
+        [`btn-${size}`]: size,
+        'disabled': (btnType === ButtonType.Link) && disabled
+    });
+
+    if (btnType === ButtonType.Link) {
+        return (
+            <a className={classes}>
+                {children}
+            </a>
+        )
+    } else {
+        return (
+            <button className={classes} disabled={disabled}>
+                {children}
+            </button>
+        )
+    }
+}
+
+export default Button;
